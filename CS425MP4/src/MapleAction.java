7f3d576933ce44f5910175e9dad4e5328087e883
abhishekes <abhishek.es@gmail.com>
2012-11-28 19:13:55 -0600

    Initial Commit

---
+++ CS425MP4/src/MapleAction.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+
+public class MapleAction {
+	Integer             mapleTaskId;
+	Integer             machineId;
+	String              mapleExe;
+	ArrayList<String[]> inputFileInfo;
+	String              outputFilePrefix; 
+	
+
+}
---
+++ CS425MP4/src/MapleJuicePayload.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class MapleJuicePayload {
+
+	int           messageType;
+	int           messageLength;
+	byte[]        payload;
+
+}
