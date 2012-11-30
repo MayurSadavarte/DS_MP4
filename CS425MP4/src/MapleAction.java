b7de118a6b3bea5c4e58330f320745a1aa59b51f
MayurSadavarte <mayoorsada@gmail.com>
2012-11-29 23:00:47 -0600

    mapleAction and MapleJuicePayload inital design

--- CS425MP4/src/MapleAction.java
+++ CS425MP4/src/MapleAction.java
@@ -1,4 +1,9 @@
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.*;
 
 //Payload for sending Maple Action Payload to worker nodes
@@ -10,4 +15,52 @@ public class MapleAction {
 	String              outputFilePrefix; 
 	
 
+	
+	public byte[] getByteArray() {
+		byte[] op = new byte[1016];
+		ByteArrayOutputStream baos=null;
+		ObjectOutputStream oos=null;
+		baos = new ByteArrayOutputStream();
+		
+		try {
+			oos = new ObjectOutputStream(baos);
+			oos.writeObject(this);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return(baos.toByteArray());
+	}
+	
+	public void parseByteArray(byte[] ip) {
+		ByteArrayInputStream bais = new ByteArrayInputStream(ip);
+		try {
+			ObjectInputStream oos = new ObjectInputStream(bais);
+			MapleAction dummy=null;
+			try {
+				dummy = (MapleAction)oos.readObject();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			mapleTaskId = dummy.mapleTaskId;
+			machineId = dummy.machineId;
+			mapleExe = dummy.mapleExe;
+			inputFileInfo = dummy.inputFileInfo;
+			outputFilePrefix = dummy.outputFilePrefix;
+			
+			System.out.println(mapleTaskId);
+			System.out.println(machineId);
+			System.out.println(mapleExe);
+			System.out.println(inputFileInfo);
+			System.out.println(outputFilePrefix);
+			
+			//this = new MapleAction();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
--- CS425MP4/src/MapleJuicePayload.java
+++ CS425MP4/src/MapleJuicePayload.java
@@ -1,9 +1,16 @@
+import java.io.Serializable;
 import java.util.*;
 //Basic Payload
-public class MapleJuicePayload {
-
+public class MapleJuicePayload implements Serializable{
 	int           messageType;
 	int           messageLength;
 	byte[]        payload;
+	
+	public MapleJuicePayload(int mt, int ml, byte[] data) {
+		messageType = mt;
+		messageLength = ml;
+		payload = data;
+	}
+	
 
 }
