945369305aa35ba643d6204fe17acfdaf368e29d
abhishekes <abhishek.es@gmail.com>
2012-12-05 00:49:49 -0600

    Added Node failure handling. May have to revisit

--- CS425MP4/src/MapleJuiceListener.java
+++ CS425MP4/src/MapleJuiceListener.java
@@ -10,75 +10,118 @@ public class MapleJuiceListener implements Runnable {
 	private Machine m;
 	public static int task_id;
 	public static HashMap<Integer , HashMap<String, Process>> task_map; 
+	public static HashMap<String, ArrayList<String>> master_task_map;
+	Vector<String> freeNodeList;
+	Vector<String> pendingFileList;
+	
 	public void start()
 	{
 		Thread server_thread = new Thread(this);
-		
+
 		server_thread.start();
 	}
 	public MapleJuiceListener(Machine machine) {
 		task_map = new HashMap<Integer, HashMap<String, Process>>();
+		master_task_map = new HashMap<String, ArrayList<String>>();
 		m = machine;
 		task_id = 0;
 	}
 	@SuppressWarnings("unchecked")
 	public void processMapleCommand(String mapleExe, Vector<String> filesToProcess, String outputFilePrefix) {
-		@SuppressWarnings("rawtypes")		
-		ArrayList[] nodeFileList = new ArrayList[m.memberList.size()];
-		for (int i = 0 ; i < m.memberList.size(); i++) {
-			nodeFileList[i] = new ArrayList<String>();
 
-		}
-		int i = 0;
+
+		freeNodeList = new Vector<String>(m.memberList);
+		pendingFileList = new Vector<String>(filesToProcess);
+		boolean tasksComplete = false;
 		task_id++;
-		for (String fileName : filesToProcess) {
-			nodeFileList[i].add(fileName);
-			i = (i + 1) % m.memberList.size();
+		@SuppressWarnings("rawtypes")
+		ArrayList[] nodeFileList = null;
+		while (!tasksComplete && pendingFileList != null && pendingFileList.size() > 0) {
 
-		}
-		for (int j = 0 ; ((j < m.memberList.size()) && (nodeFileList[j].size()>0)); j++) {
-			MapleAction temp = new MapleAction();
-			temp.mapleTaskId = task_id;
-			temp.machineId = j + 1;
-			temp.mapleExe = mapleExe;
-			temp.inputFileInfo = nodeFileList[j];
-			temp.outputFilePrefix = outputFilePrefix;
-			MapleJuicePayload mj_payload = new MapleJuicePayload("MapleTask");
-			mj_payload.setByteArray(temp);
-			mj_payload.sendMapleJuicePacket(m.memberList.elementAt(j), false);
-		}
 
-		
-		TaskStatus status = new TaskStatus();
-		status.taskId = task_id;
-		status.messageType = new String("get");
-		//Monitor the progress on the node every 10 seconds
-		for (int j = 0 ; ((j < m.memberList.size()) && (nodeFileList[j].size()>0)); j++) {
-			 
-			MapleJuicePayload mj_payload = new MapleJuicePayload("TaskStatus");
-			
-			mj_payload.setByteArray(status);
-			Socket sendSocket = mj_payload.sendMapleJuicePacket(m.memberList.elementAt(j), true);
-			mj_payload.receiveMapleJuicePacket(sendSocket);
-			try {
-				Thread.sleep(10 * 1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			if (pendingFileList.size() > 0 && freeNodeList.size() > 0) {
+
+				nodeFileList = new ArrayList[freeNodeList.size()];
+				for (int i = 0 ; i < pendingFileList.size() && i < freeNodeList.size(); i++) {				
+					nodeFileList[i] = new ArrayList<String>();
+				}
+
+				int i = 0;	
+				for (String fileName : pendingFileList) {
+
+					nodeFileList[i].add(fileName);
+					i = (i + 1) % freeNodeList.size();
+					pendingFileList.remove(fileName);
+
+				}
+				for (int j = 0 ; ((j < freeNodeList.size()) && (nodeFileList[j].size() > 0)); j++) {
+					MapleAction temp = new MapleAction();
+					temp.mapleTaskId = task_id;
+					temp.machineId = j + 1;
+					temp.mapleExe = mapleExe;
+					temp.inputFileInfo = nodeFileList[j];
+					temp.outputFilePrefix = outputFilePrefix;
+					MapleJuicePayload mj_payload = new MapleJuicePayload("MapleTask");
+					mj_payload.setByteArray(temp);
+					mj_payload.sendMapleJuicePacket(freeNodeList.elementAt(j), false);
+					if (!master_task_map.containsKey(freeNodeList.elementAt(j))) {
+						master_task_map.put(freeNodeList.elementAt(j), nodeFileList[j]);
+					}
+					freeNodeList.remove(j);
+					//master_task_map.get(m.memberList.elementAt(j)).add(nodeFileList[j]);
+				}
+			}
+
+			TaskStatus status = new TaskStatus();
+			status.taskId = task_id;
+			status.messageType = new String("get");
+			//Monitor the progress on the node every 10 seconds
+			tasksComplete = true;
+			for (String nodeName : master_task_map.keySet() ) {
+               
+				MapleJuicePayload mj_payload = new MapleJuicePayload("TaskStatus");
+
+				mj_payload.setByteArray(status);
+				
+				Socket sendSocket = mj_payload.sendMapleJuicePacket(nodeName, true);
+				mj_payload.receiveMapleJuicePacket(sendSocket);
+				TaskStatus receivedStatus = (TaskStatus) mj_payload.parseByteArray();
+				//Print the obtained results
+				boolean mapsCompletedOnNode = true;
+				System.out.println("Status on node " + nodeName +  " :");
+				for (String fileName : receivedStatus.taskStatus.keySet())
+				{
+					System.out.println("Filename : " + fileName + " Status : " + receivedStatus.taskStatus.get(fileName));
+					if (receivedStatus.taskStatus.get(fileName).equals("In progress")) {
+						mapsCompletedOnNode = false;
+						tasksComplete = false;
+					}else if (receivedStatus.taskStatus.get(fileName).equals("Failed")) {
+						pendingFileList.add(fileName);
+					}
+				}
+				if (mapsCompletedOnNode) {
+					freeNodeList.add(nodeName);
+					master_task_map.remove(nodeName);
+				}
+				try {
+					Thread.sleep(10 * 1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
-		
-		
 
 
-}
+
+	}
 
 
 	public void processJuiceCommand(String sdfsExe, int num_reducers, String outputsdfsFileName, String outputFilePrefix) {
-		
+
 	}
-	
-	
+
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
